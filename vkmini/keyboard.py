28f9824345556c0b7a61c104f06b5c4dd386df43
Elchinchel <Elchin751@gmail.com>
2022-11-26 00:37:56 +0300

    0.6.0a1 Клавиатуре запилено нормальное форматирование

--- vkmini/keyboard.py
+++ vkmini/keyboard.py
@@ -1,6 +1,14 @@
 import json
 
-from typing import List, Union, Literal
+from enum import Enum
+from typing import Any, List, Dict, Union, Literal
+
+
+__all__ = (
+    'Button',
+    'FormattableButton',
+    'Keyboard'
+)
 
 
 ButtonColors = Literal[
@@ -20,6 +28,20 @@ ButtonTypes = Literal[
 
 
 class Button:
+    class Color(str, Enum):
+        PRIMARY = 'primary'      # Синяя, обычная
+        SECONDARY = 'secondary'  # "Бледная"
+        POSITIVE = 'positive'    # Зелёная
+        NEGATIVE = 'negative'    # Красная
+
+    class Type(str, Enum):
+        TEXT = 'text'
+        VKPAY = 'vkpay'
+        LOCATION = 'location'
+        CALLBACK = 'callback'
+        OPEN_APP = 'open_app'
+        OPEN_LINK = 'open_link'
+
     payload: str
     color: str
     label: str
@@ -29,8 +51,8 @@ class Button:
             self,
             label: str,
             payload: Union[str, dict],
-            type: ButtonTypes = 'text',
-            color: ButtonColors = 'primary'
+            type: Type = Type.TEXT,
+            color: Color = Color.PRIMARY
     ):
         self.payload = payload
         if isinstance(self.payload, dict):
@@ -40,7 +62,13 @@ class Button:
         self.type = type
 
     def __str__(self) -> str:
-        return json.dumps(self.obj, ensure_ascii=False)
+        return Keyboard(self).jsonize()
+
+    def __format_button__(self, data: Dict[str, Any]) -> 'Button':
+        return self
+
+    def copy(self) -> 'Button':
+        return Button(self.label, self.payload, self.type, self.color)
 
     @property
     def obj(self):
@@ -52,6 +80,40 @@ class Button:
             },
             'color': self.color
         }
+
+
+class FormattableButton(Button):
+    """
+    Позволяет не создавать клавиатуру каждый раз, когда нужно поменять
+    пару параметров в `payload` или `label` кнопки.
+
+    Форматирование осуществляется вызовом метода `.format` у объектов
+    `payload` и `label`, для переопределения способа форматирования
+    можно переопределить метод `__format_button__` у этого класса или
+    при создании кнопки передавать вместо `str` экземпляры своего класса
+
+    Использование:
+    ```
+    keyboard = Keyboard([
+        Button('Обычная кнопка', 'статичная нагрузочка'),
+        FormattableButton('Label с {label_var}', 'Payload с {payload_var}')
+    ])
+
+    data = {
+        'label_var': 'ярлычком',
+        'payload_var': 'нагрузочкой'
+    }
+    formatted_kb = keyboard.format(data)  # вернёт клавиатуру с форматированными кнопками
+
+    formatted_kb.jsonize()
+    >>> ... {"label": "Label с ярлычком", "payload": "Payload с нагрузочкой"}
+    ```
+    """
+    def __format_button__(self, data: Dict[str, Any]) -> 'Button':
+        return Button(self.label.format(**data),
+                      self.payload.format(**data),
+                      self.type,
+                      self.color)
 
 
 class Keyboard:
@@ -69,47 +131,41 @@ class Keyboard:
         if buttons is not None:
             self.add_buttons(buttons)
         if inline and one_time:
-            raise ValueError(
-                'inline и one_time -- взаимоисключающие параметры'
-            )
+            raise ValueError('inline и one_time -- взаимоисключающие параметры')
         self.inline = inline
         self.one_time = one_time
 
     def __str__(self) -> str:
         return self.jsonize()
 
+    def add_new_button(self,
+                       label: str,
+                       payload: Union[str, dict],
+                       type: Button.Type = Button.Type.TEXT,
+                       color: Button.Color = Button.Color.PRIMARY):
+        self.add_buttons(Button(label, payload, type, color))
+
     def add_buttons(self, buttons: Union[Button, List[Button]]):
-        # TODO: возможность использовать описание кнопки вместо объекта
         if isinstance(buttons, Button):
             buttons = [buttons]
         self.buttons.append(buttons)
 
-    def jsonize(self, alt_buttons: List[List[Button]] = None) -> str:
-        # XXX: зачем здесь аргумент alt_buttons?
-        if alt_buttons is None:
-            alt_buttons = self.buttons
+    def jsonize(self) -> str:
         return json.dumps({
             'one_time': self.one_time,
             'inline': self.inline,
-            'buttons': [[b.obj for b in li] for li in alt_buttons]
+            'buttons': [[btn.obj for btn in line] for line in self.buttons]
         }, ensure_ascii=False)
 
-    def format_label(self, data: Union[tuple, dict]) -> 'Keyboard':
-        # XXX: это должно быть в кнопках
-        for line in self.buttons:
-            for button in line:
-                try:
-                    button.label = button.label % data
-                except TypeError:
-                    pass
-        return self
+    def copy(self) -> 'Keyboard':
+        return [[btn.copy() for btn in line] for line in self.buttons]
 
-    def format_payload(self, data: Union[tuple, dict]) -> 'Keyboard':
-        # XXX: это должно быть в кнопках
-        for line in self.buttons:
-            for button in line:
-                try:
-                    button.payload = button.payload % data
-                except TypeError:
-                    pass
-        return self
+    def format(self, data: Dict[str, Any]) -> 'Keyboard':
+        formatted_keyboard = Keyboard()
+        for btn_line in self.buttons:
+            formatted_keyboard.add_buttons(
+                [btn.__format_button__(data) for btn in btn_line]
+            )
+        return formatted_keyboard
+
+    format.__doc__ = FormattableButton.__doc__
